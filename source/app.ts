409592377c479fdc1928f792a1cefe09ecd13ff7
arif.dwi <arif.dwi@peruridigit.co.id>
2025-07-24 13:29:09 +0700

    fix: cors access from all origin

--- source/app.ts
+++ source/app.ts
@@ -33,8 +33,6 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
   });
 });
 
-app.use(cors()); // Open CORS policy (vulnerability)
-
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
