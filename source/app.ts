7164db8065ffb855c7f826cc2f11e1809600c007
arif.dwi <arif.dwi@peruridigit.co.id>
2025-07-24 13:43:54 +0700

    add code smell with use open CORS policy with full access for all origin

--- source/app.ts
+++ source/app.ts
@@ -33,6 +33,8 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
   });
 });
 
+app.use(cors()); // Open CORS policy (vulnerability)
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
